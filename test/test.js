f64ee8dc4d768d5315433d7a88d6731a67da5315
Rahul16-creator <rahulkannanrk1999@gmail.com>
2021-05-03 14:14:10 +0530

    updated in schedule service

--- test/test.js
+++ test/test.js
@@ -1,2 +1,7 @@
 const moment = require("moment")
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+let data = "Thu Jun 28 2018 21:30:00 GMT+0530 (India Standard Time)"
+
+console.log(moment(new Date(data), "YYYY-MM-DD HH:mm").format("MMMM Do YYYY_h:mm A"))
+
+console.log(new Date(data))
