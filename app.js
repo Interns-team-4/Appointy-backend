6914f9de8f872f0d087cc44905ad0b7f550b8e6b
Rahul16-creator <rahulkannanrk1999@gmail.com>
2021-05-08 00:12:42 +0530

    body_parser removed

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.set('views', 'views')
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 app.use("/api/v1", userRouter);
 app.use("/api/v1", scheduleRouter)
